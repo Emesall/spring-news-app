0aa56a534b5b4399a80fc0ea69279616e4f8c896
Emesall <mati1230@gmail.com>
2022-08-14 09:14:45 +0200

    Block pages, #32 closed.

--- src/main/java/com/emesall/news/config/SecurityConfig.java
+++ src/main/java/com/emesall/news/config/SecurityConfig.java
@@ -28,13 +28,19 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-		.antMatchers("/settings")
+		.antMatchers("/settings/**","/list/**")
 		.authenticated()
 		.antMatchers("/")
 		.permitAll()
 		.and()
 		.formLogin()
-		.loginPage("/login");
+		.loginPage("/login")
+		.and()
+		.logout()
+		.deleteCookies("JSESSIONID")
+		.and()
+		.rememberMe()
+		.key("uniqueAndSecret");
 		return http.build();
 	}
 	/*
