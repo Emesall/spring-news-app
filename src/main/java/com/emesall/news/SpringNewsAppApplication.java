103748c09cc5980b0e3cd9bc76eba48c59c46d9f
Emesall <mati1230@gmail.com>
2022-09-21 19:21:53 +0200

    Certificate(https), email change

--- src/main/java/com/emesall/news/SpringNewsAppApplication.java
+++ src/main/java/com/emesall/news/SpringNewsAppApplication.java
@@ -2,14 +2,20 @@ package com.emesall.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class SpringNewsAppApplication {
+public class SpringNewsAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringNewsAppApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringNewsAppApplication.class);
+    }
 }
--- src/main/java/com/emesall/news/service/SimpleEmailSender.java
+++ src/main/java/com/emesall/news/service/SimpleEmailSender.java
@@ -1,16 +1,21 @@
 package com.emesall.news.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import lombok.Setter;
+
 @Service
+@Setter
+@ConfigurationProperties(prefix = "email")
 public class SimpleEmailSender{
 
 	private JavaMailSender mailSender;
-	
+	private String sender_email;
 	
 	@Autowired
 	public SimpleEmailSender(JavaMailSender mailSender) {
@@ -23,6 +28,7 @@ public class SimpleEmailSender{
 	public void sendEmail(String emailAddress, String subject, String message) throws MailException{
 		
 		SimpleMailMessage email = new SimpleMailMessage();
+		email.setFrom(sender_email);
 		email.setTo(emailAddress);
 		email.setSubject(subject);
 		email.setText(message);
